46553828dbfbbab37a13637a2e352062dfe3bbad
Tomazzo Antonioli <tomazzo10@hotmail.com>
2022-02-20 19:01:12 +0100

    cleaning

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,5 @@
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
-
 import Header from "./components/Header/Header";
 import ProductsList from "./components/ProductsList/ProductsList";
-// import FavouriteModal from "./components/ProductsList/FavouritesModal/FavouriteModal";
-
-// const actions = {
-//   toggleFilter: (): string => "none",
-//   toggleFavouriteModal: (): boolean => false,
-// };
-
-// interface Props extends StateProps, DispatchProps {}
 
 function App() {
   return (
@@ -24,29 +13,5 @@ function App() {
     </div>
   );
 }
-
-// interface StateProps {
-//   flag: boolean;
-// }
-
-// interface DispatchProps {
-//   toggleFilter: typeof actions.toggleFilter;
-//   toggleFavouriteModal: typeof actions.toggleFavouriteModal;
-// }
-
-// // To do: Right type in state and dispatch
-
-// const mapStateToProps = (state: any): StateProps => ({
-//   flag: state.toggleFavouriteModalReducer.flag,
-// });
-
-// const mapDispatchToProps = (dispatch: any) => ({
-//   ...bindActionCreators(
-//     {
-//       ...actions,
-//     },
-//     dispatch
-//   ),
-// });
 
 export default App;
